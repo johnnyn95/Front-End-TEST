74588d3de70ae8dc14415988e3e604f635253093
Jonathan Nguyen <JNguyen_student@paraflow.bg>
2017-01-27 16:36:03 +0200

    up

--- js/testing.js
+++ js/testing.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-    $('#welcome').fadeIn('slow');
+    $('#welcome')
 });
 
 $('#welcome').hover(function() {
     $('#welcome').css({ "color": "yellow", "font-size": "50px" });
+});
+
+$("div#logo").click(function() {
+    $("div#menu-popup").css("display", "block");
+})
+$("div#menu-popup").mouseout(function() {
+    $("div#menu-popup").css("display", "none");
+});
+
+
+$("div#convertions").hover(function() {
+    $("div#convertions ul#temperature").css("display", "block");
+    $("div#convertions ul#weigth").css("display", "block");
+});
+$("div#convertions").mouseout(function() {
+    $("div#convertions ul#temperature").css("display", "none");
+    $("div#convertions ul#weigth").css("display", "none");
 });
