be22f8eb343183d6c00d4229fe1b412a28ac97ea
Jonathan Nguyen <JNguyen_student@paraflow.bg>
2017-02-03 17:51:01 +0200

    up

--- js/testing.js
+++ js/testing.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    /*
     $("#logo *").hover(function() {
         $("#logo>a>img").attr("src", "/imgs/logo-cube.png");
     });
@@ -7,11 +8,62 @@ $(document).ready(function() {
     });
 
     $(".page-header  *").hover(function() {
+        $("#welcome").fadeOut(2000, "swing");
+    });
+    */
+
+    /* thumbnails  */
+    $("a#tmb_currency>*").mouseenter(function() {
+        $("p#tmb_currency*").fadeOut(1000);
+    });
+    $("a#tmb_currency> ").mouseleave(function() {
+        $("p#tmb_currency ").fadeIn(1000);
+    });
+
+    $("a#tmb_weight>*").hover(function() {
+        $("p#tmb_weight").fadeOut(1000);
+
+    });
+    $("a#tmb_weight>*").mouseout(function() {
+        $("p#tmb_weight").fadeIn(1000);
+    });
+
+    $("a#tmb_length>*").hover(function() {
+        $("p#tmb_length").fadeOut(1000);
+
+    });
+    $("a#tmb_length>*").mouseout(function() {
+        $("p#tmb_length").fadeIn(1000);
+    });
+
+    $("a#tmb_area>*").hover(function() {
+        $("p#tmb_area").fadeOut(1000);
+
+    });
+    $("a#tmb_area>*").mouseout(function() {
+        $("p#tmb_area").fadeIn(1000);
+    });
+
+    $("a#tmb_volume>*").hover(function() {
+        $("p#tmb_volume").fadeOut(1000);
+
+    });
+    $("a#tmb_volume>*").mouseout(function() {
+        $("p#tmb_volume").fadeIn(1000);
+    });
+
+
+
+
+    /* welcome test
+    $(".page-header  *").hover(function() {
         $(".jumbotron").css("background-color", "gray");
         $(".jumbotron>h1").css("color", "white");
     });
+    
     $(".page-header  *").mouseout(function() {
         $(".jumbotron").removeAttr('style');
         $(".jumbotron>h1").removeAttr('style');
     });
+    */
 });
